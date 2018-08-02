c3f15b82abb2a429902b34da5c8df649f93a0a03
Aimerfan <jvuchein@gmail.com>
2018-08-03 03:08:38 +0800

    Add progress bar in Updater

--- Updater.py
+++ Updater.py
@@ -1,13 +1,28 @@
 import requests
+import sys
 
-req = requests.Session()
-dlink = req.get('https://github.com/fcu-d0441320/iLearnBackupTool/raw/master/iLearnBackupTool.exe')
+durl = 'https://raw.githubusercontent.com/fcu-d0441320/iLearnBackupTool/master/iLearnBackupTool.exe'
+with requests.get(durl, stream=True) as dlink, open('iLearnBackupTool.exe', 'wb') as binfile:
+    # http error detected
+    if dlink.status_code != 200:
+        print('http error {code}.'.format(code=dlink.status_code()))
+        exit(dlink.status_code)
 
-if dlink.status_code != 200:
-    print('http error {code}.'.format(code=dlink.status_code()))
-    exit(1)
+    # initial download record paramater
+    total_size = int(dlink.headers['Content-Length'])
+    batch_size = int(total_size / 50)
+    recived_size = 0
 
-with open('iLearnBackupTool.exe', 'wb') as binfile:
-    binfile.write(exefile.content)
+    # start download
+    sys.stdout.write('[{:50s}] {:d}%'.format('', 0))
+    # print per. 2% content recived
+    for i, content in enumerate(dlink.iter_content(chunk_size=batch_size)):
+        sys.stdout.write('\r[{:<50s}] {:d}%'.format('='*i, i*2))
+        sys.stdout.flush()
+        # update contect has recived
+        recived_size += binfile.write(content)
+        if recived_size == total_size:
+            break
+    print('')
 
-print('Success')
+print('Update Success.')
